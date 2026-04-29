6414120c991bb857b2c6585f4b7042cb6dd96f93
Harsh Shukla <harshshukla24112000@gmail.com>
2026-04-29 22:29:15 +0530

    two sum with hashmap

--- TwoSum.java
+++ TwoSum.java
@@ -1,41 +1,22 @@
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class TwoSum {
     static boolean TSum(int arr[] , int target)
     {
-        Arrays.sort(arr);
-        int left = 0,right=arr.length-1;
-        while(left<right)
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<arr.length;i++)
         {
-            int sum = arr[left]+arr[right];
-            if(sum==target) return true;
-            else if(sum<target)
+            int complement = target-arr[i];
+
+            if(set.contains(complement))
             {
-                left++;
-            }else{
-                right--;
+                return true;
+            }
+            else{
+                set.add(arr[i]);
             }
         }
         return false;
-
-        /*HashSet<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < arr.length; i++) {
-
-            // Calculate the complement that added to
-            // arr[i], equals the target
-            int complement = target - arr[i];
-
-            // Check if the complement exists in the set
-            if (set.contains(complement)) {
-                return true;
-            }
-
-            // Add the current element to the set
-            set.add(arr[i]);
-        }
-        // If no pair is found
-        return false; */
     }
     public static void main(String[] args) {
         int arr[] = {2,3,5,3,5,5,1};
